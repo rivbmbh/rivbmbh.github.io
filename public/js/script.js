8df4d4dc68f8cb1dac8ef589ea31107ee2a915b5
angelRAB13 <rivaldoangelobomboah@gmail.com>
2024-02-15 10:49:29 +0800

    memperbaiki dropdown ketika di large screen ada dan dibawah large di hidden

--- public/js/script.js
+++ public/js/script.js
@@ -23,8 +23,12 @@ window.onscroll = function() {
     var timeout = setTimeout(function() {
         header.classList.remove('nav-fixed');
         header.classList.add('nav-fixed2');
-    }, 1200); // Mengubah 200 menjadi 400 untuk menambahkan delay 400ms
+    }, 500); // Mengubah 200 menjadi 400 untuk menambahkan delay 400ms
+    
+    // Menambahkan transisi ease-in-out
+    header.style.transition = "bottom 0.4s ease-in-out"; // Kamu bisa menyesuaikan nilai 0.3s sesuai kebutuhan
 }
+
 
 
 // window.onscroll = function(){
