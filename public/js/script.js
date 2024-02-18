edeb83353117439698b45e08fa2cb50b1b2f3ee7
angelRAB13 <rivaldoangelobomboah@gmail.com>
2024-02-18 15:51:30 +0800

    fitur dark level 1

--- public/js/script.js
+++ public/js/script.js
@@ -7,28 +7,22 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden');
 });
 
-//header scroll
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const up = document.querySelector('#up');
-    const fixedNav = header.offsetTop;
-
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('nav-fixed');
-        up.classList.remove('hidden');
-
-    }else{
-        header.classList.remove('nav-fixed');
-        up.classList.add('hidden');
-
+//click window agar ketika di klik dimanapun humburger tidak akan tertutup dan tetap terbuka
+window.addEventListener('cick', function() {
+    //jika yang diklick selain humbergenya 
+    if(e.taget != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
     }
-}
+})
 
 // JavaScript untuk menampilkan dan menyembunyikan dropdown saat hover
     const parentDropdown = document.querySelector('.li-down');
     const itemDropdown = document.querySelector('#dropdown');
     const faRight = document.getElementById('i-right');
     const faDown = document.getElementById('i-down');
+    const faRight2 = document.getElementById('i-right2');
+    const faDown2 = document.getElementById('i-down2');
 
     
 
@@ -40,6 +34,14 @@ window.onscroll = function(){
         // for caret-down
         faDown.classList.remove('hidden');
         faDown.classList.add('inline');
+
+
+        //WHITE
+        faRight2.classList.remove('dark:inline');
+        faRight2.classList.add('dark:hidden');
+        // for caret-down
+        faDown2.classList.remove('dark:hidden');
+        faDown2.classList.add('dark:inline');
     });
     
     parentDropdown.addEventListener('mouseleave', function() {
@@ -50,5 +52,53 @@ window.onscroll = function(){
         // for caret-down
         faDown.classList.remove('inline');
         faDown.classList.add('hidden');
+
+        //WHITE
+        faRight2.classList.remove('dark:hidden');
+        faRight2.classList.add('dark:inline');
+        // for caret-down
+        faDown2.classList.remove('dark:inline');
+        faDown2.classList.add('dark:hidden');
     });
     
+//header scroll
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const up = document.querySelector('#up');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('nav-fixed');
+        up.classList.remove('hidden');
+        itemDropdown.classList.add('nav-dropdownScroll');
+
+    }else{
+        header.classList.remove('nav-fixed');
+        up.classList.add('hidden');
+        itemDropdown.classList.remove('nav-dropdownScroll');
+        itemDropdown.classList.add('nav-dropdown');
+    }
+}
+
+
+
+//DarkMode
+function toggleDarkMode(){
+    if(localStorage.getItem("theme") === "dark"){
+        localStorage.setItem("theme", "light");
+        document.documentElement.classList.remove("dark");
+    }else{
+        localStorage.setItem("theme", "dark");
+        document.documentElement.classList.add("dark");
+    }
+}
+
+
+document
+    .querySelector("#darkMode")
+    .addEventListener("click", toggleDarkMode);
+    if(localStorage.getItem("theme") === "dark"){
+        document.documentElement.classList.add("dark");
+    }
+
+//END DARK MODE FUNCTION
